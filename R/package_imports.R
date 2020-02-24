797ae2a83c7032f75a3bb13d85b8dfacb8fcb26d
collin <collin_nolte@uiowa.edu>
2020-02-24 13:59:25 -0600

    import packages

--- R/package_imports.R
+++ R/package_imports.R
@@ -1,6 +1,7 @@
 #' @importFrom data.table dcast
 #' @importFrom data.table as.data.table
 #' @importFrom data.table is.data.table
+#' @importFrom data.table rbindlist
 #' @import DescTools
 #' @import magrittr
 #' @import stringr
